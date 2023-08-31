52b72f70907d49ebc8c8ff32f7bdc2cb7ba24fc1
Mux <muxxum65536@gmail.com>
2023-08-31 16:06:38 +0800

    add basic parts of auto-grad

--- DeepZero/__init__.py
+++ DeepZero/__init__.py
@@ -1,6 +1,2 @@
-from .DeepZeroModule import DeepModule
-from ._Linear import Linear
-from .ModuleSeqence import ModuleSequence
-
-
-__all__ = ['DeepModule', 'Linear', 'ModuleSequence']
+from .DeepZeroVar import DeepZeroVar
+from .functions import *
